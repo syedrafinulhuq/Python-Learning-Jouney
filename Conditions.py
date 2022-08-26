2f34eac60f8f167876997a4e8f7d7103e920d7c5
A. M. Rafinul Huq <85757029+syedrafinulhuq@users.noreply.github.com>
2022-08-26 14:41:59 +0600

    Add files via upload

---
+++ Conditions.py
@@ -0,0 +1,8 @@
+# In this session I'm learning conditions and how to chain conditions.
+
+ # first I'm taking input
+inp = input("Enter the number")
+
+
+
+
---
+++ Variable.py
@@ -0,0 +1,9 @@
+# here we are taking two variables (a and b ) and initializing them with values
+
+a = 5;
+b = 6;
+
+# Now we are performing mathematical operation by add two variables
+# The output will be 11 because as per we are adding two varibale values so the output will be a number
+
+print(a+b)
